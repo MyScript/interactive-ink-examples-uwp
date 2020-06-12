9d7e7dfe3176f839d8ecd5d8765f3db75e09d864
MyScript <packages@myscript.com>
2020-06-12 14:02:07 +0200

    Update MyScript.InteractiveInk.Uwp to version 1.4.0

--- GetStarted/Properties/AssemblyInfo.cs
+++ GetStarted/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MyScript")]
 [assembly: AssemblyProduct("MyScript.InteractiveInk.GetStarted.Uwp")]
-[assembly: AssemblyCopyright("Copyright ©  2017, 2018")]
+[assembly: AssemblyCopyright("Copyright MyScript. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
 [assembly: ComVisible(false)]
