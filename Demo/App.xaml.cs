544e3360cd99cea676a0d8f3a3a42237dd633acf
MyScript <packages@myscript.com>
2022-05-20 12:45:11 +0200

    Add support for typeset and gestures in raw content part and block

--- Demo/App.xaml.cs
+++ Demo/App.xaml.cs
@@ -32,6 +32,35 @@ namespace MyScript.IInk.Demo
             var localFolder = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
             var tempFolder = System.IO.Path.Combine(localFolder, "tmp");
             engine.Configuration.SetString("content-package.temp-folder", tempFolder);
+
+            EnableRawContentConversion(engine);
+        }
+
+        private static void EnableRawContentConversion(Engine engine)
+        {
+            // Activate handwriting recognition for text and shapes
+            engine.Configuration.SetBoolean("raw-content.recognition.text", true);
+            engine.Configuration.SetBoolean("raw-content.recognition.shape", true);
+
+            // Allow conversion of text, nodes and edges
+            engine.Configuration.SetBoolean("raw-content.convert.node", true);
+            engine.Configuration.SetBoolean("raw-content.convert.text", true);
+            engine.Configuration.SetBoolean("raw-content.convert.edge", true);
+
+            // Allow converting shapes by holding the pen in position
+            engine.Configuration.SetBoolean("raw-content.convert.shape-on-hold", true);
+
+            // Allow interactions
+            engine.Configuration.SetBoolean("raw-content.tap-interactions", true);
+            engine.Configuration.SetBoolean("raw-content.eraser.erase-precisely", false);
+
+            // Show alignment guides and snap to them
+            engine.Configuration.SetBoolean("raw-content.guides.enable", true);
+            engine.Configuration.SetBoolean("raw-content.guides.snap", true);
+
+            // Allow gesture detection
+            var gestures = new string[] { "underline", "double-underline", "scratch-out", "join", "insert", "strike-through" };
+            engine.Configuration.SetStringArray("raw-content.pen.gestures", gestures);
         }
 
         private static async void OnUnhandledException(object sender, Windows.UI.Xaml.UnhandledExceptionEventArgs e)
